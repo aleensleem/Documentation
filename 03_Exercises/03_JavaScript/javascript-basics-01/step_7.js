64839d5e6f1ade6acaf72871fd0cd70ab883e294
aleen sleem <aleensleem29@gmail.com>
2022-12-07 13:14:51 +0200

    step7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_7.js
@@ -0,0 +1,15 @@
+var test1 = prompt("Enter shoe size" );
+var test2 = prompt("Enter birth year" );
+
+
+function function1 (test1,test2){
+var x = test1 * 2
+var y = x +5
+var a = y * 50
+var b = a - test2
+var c = b + 1766
+
+return alert(c);
+}
+
+function1(test1, test2);
