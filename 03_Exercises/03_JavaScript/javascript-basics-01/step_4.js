bd0daf1b5a3b06aa41973ccb1e13a982d96142e0
aleen sleem <aleensleem29@gmail.com>
2022-12-07 10:59:52 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,5 @@
+ballout1 = prompt("Enter you name:");
+ballout2 = prompt("Enter you surname:");
+ballout3 = prompt("Enter you city:");
+
+alert("Name: " + ballout1  +"\n" + "Surname: " + ballout2 +"\n" + "City: " + ballout3 + "\n");
