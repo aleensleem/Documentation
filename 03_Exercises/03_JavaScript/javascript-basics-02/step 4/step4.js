3ffdf69fa3470ad095722674b8834f1be6e0c399
aleen sleem <aleensleem29@gmail.com>
2022-12-09 07:04:42 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step4.js
@@ -0,0 +1,12 @@
+function myFunction(){
+  const name = document.querySelector("#name");
+  const surname = document.querySelector("#surname");
+  const city = document.querySelector("#city");
+
+  var answer = confirm("Clear all fields ?");
+        if (answer) {
+          name.value = " ";
+          surname.value = " ";
+          city.value = " ";
+        }
+}
