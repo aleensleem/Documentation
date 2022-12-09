3b1ef554ff39b4dae5029e0f49b7bb37bb530f2e
aleen sleem <aleensleem29@gmail.com>
2022-12-09 06:48:54 +0200

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 3/step3.js
@@ -0,0 +1,8 @@
+function func(){
+    const name = document.querySelector("#name")
+    const text = document.querySelector("#text")
+
+   var hh = name.value ;
+   text.innerHTML = hh ; 
+   name.value = " " ;
+}
