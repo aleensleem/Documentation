47f32012e4318c991e5e37042b3971f3346f6dbd
aleen sleem <aleensleem29@gmail.com>
2022-12-09 12:55:06 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 4/step_4.js
@@ -0,0 +1,22 @@
+var input=document.getElementById('password').value;
+var conf=document.getElementById('confirmation').value;
+var butt=document.querySelector('button')
+
+
+butt.addEventListener("click",function(){
+match();
+});
+
+
+
+
+function match(){
+    
+    if( input= confirmation) {
+        password.style.border = ' ';
+    
+    }else{
+      password.style.border = '2px  solid red ';
+
+}}
+    
