b1c89255025e55e17c21b35471fdb8f3a360e7de
aleen sleem <aleensleem29@gmail.com>
2022-12-09 12:20:43 +0200

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,16 @@
+
+
+  function func1(){
+    text.style.color= 'green'
+
+  }
+  function func2(){
+    text.style.color= 'red'
+    
+      }
+    
+      function func3(){
+        text.style.color= 'blue'
+        
+          }
+        
