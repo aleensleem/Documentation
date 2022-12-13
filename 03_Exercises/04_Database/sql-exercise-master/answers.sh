cf5be97963d2d91b28686455ced94c090d96b7c9
aleen sleem <aleensleem29@gmail.com>
2022-12-13 12:43:45 +0200

    database

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -24,3 +24,55 @@ where name= 'basma'
 update students
 set Points=100
 where name= 'alex'
+
+10- INSERT INTO graduates(name, Age, Gender, points)
+SELECT 
+   name,Age,Gender,Points
+FROM 
+   students
+WHERE
+   name = 'Layal';
+
+
+
+11- UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE name = 'Layal'
+
+12- DELETE FROM students
+WHERE name = 'Layal'
+
+14- FROM employees e
+INNER JOIN companies c ON e.Company=c.Name
+
+15- FROM employees e
+INNER JOIN companies c ON e.Company=c.Name
+WHERE Date<2000
+
+16- FROM employees e
+INNER JOIN companies c ON e.Company=c.Name
+WHERE Role='Graphic Designer'
+
+18- select name
+from students
+WHERE Points = (SELECT max(Points) FROM students)
+
+
+19- SELECT AVG(Points)
+FROM students;
+
+
+20-SELECT count()
+FROM students
+where Points = 500;
+
+
+21- SELECT name
+FROM students
+where name like '%s%'
+
+
+22- SELECT
+FROM students
+ORDER BY points DESC
+﻿
