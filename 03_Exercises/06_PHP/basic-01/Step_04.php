07f6a41d4d1a5cbf511eeff45684402d89425251
aleen sleem <aleensleem29@gmail.com>
2023-02-20 11:54:06 +0200

    step4

---
+++ 03_Exercises/06_PHP/basic-01/Step_04.php
@@ -0,0 +1,6 @@
+<?php
+$name = "aleen";
+$position = "Web developer";
+$github_url ="https://github.com/aleensleem";
+echo "Hello, I'm " .$name.", I'm a $position please check my github link $github_url";
+?>
