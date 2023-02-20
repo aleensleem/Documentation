8f50e5561df524d246aaa90c266bc71a56115ee9
aleen sleem <aleensleem29@gmail.com>
2023-02-20 12:13:57 +0200

    step5

---
+++ 03_Exercises/06_PHP/basic-01/Step_05.php
@@ -0,0 +1,10 @@
+<?php
+$course_name ="Wordpress";
+$enrolled_students = "28";
+$price = "free";
+$on_discount="false";
+echo "Course name is $course_name ";
+echo "Enrolled Students $enrolled_students";
+echo "Price: $price";
+echo "Course on discount: $on_discount";
+?>
