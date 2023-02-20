309daef054c6dce2f80cc9b9a0bdb7fea1026b35
aleen sleem <aleensleem29@gmail.com>
2023-02-20 11:33:25 +0200

    step3

---
+++ 03_Exercises/06_PHP/basic-01/Step_03.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Aleen";
+$email = "aleensleem29@gmail.com";
+echo "My name is $name and my email is $email";
+$height = "163";
+$width = "50";
+$area = $height*$width;
+echo "$area";
+?>
